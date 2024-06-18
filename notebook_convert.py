10ae1efc509e2816198621d2931bc76632181878
shashankkotyan <shashankkotyan@gmail.com>
2024-06-18 11:23:20 +0900

    Update

--- notebook_convert.py
+++ notebook_convert.py
@@ -2,4 +2,4 @@ import nbformat as nbf
 ntbk = nbf.read("Tutorial.ipynb", nbf.NO_CONVERT)
 new_ntbk = ntbk
 new_ntbk.cells = [cell for cell in ntbk.cells if cell.cell_type != "markdown"]
-nbf.write(new_ntbk, "Tutorial_collab.ipynb", version=nbf.NO_CONVERT)
+nbf.write(new_ntbk, "Tutorial_colab.ipynb", version=nbf.NO_CONVERT)
